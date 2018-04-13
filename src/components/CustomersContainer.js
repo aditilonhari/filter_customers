8f0d0d40179e6206599e59b968e80f927ac0a307
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-04-13 16:45:33 -0700

    Add Company filter and dropdown to UI

--- src/components/CustomersContainer.js
+++ src/components/CustomersContainer.js
@@ -6,60 +6,90 @@ class CustomersContainer extends Component {
 	  super(props)
 	  this.state = {
 	  	query: '',
-	    customers: []
+	    customers: [],
+	    company: '*'
 	  }
-	  this.handleInput = this.handleInput.bind(this);
-      this.handleQuery = this.handleQuery.bind(this);
+	  this.handleSearchInput = this.handleSearchInput.bind(this);
+//      this.handleQuery = this.handleQuery.bind(this);
+      this.handleButtonClick = this.handleButtonClick.bind(this);
+      this.handleDropdownChange = this.handleDropdownChange.bind(this);
 	}
 
 	componentDidMount() {
   		axios.get('http://localhost:3001/api/v1/customers.json')
   		.then(response => {
-    		console.log(response)
     		this.setState({customers: response.data})
   		})
   		.catch(error => console.log(error))
 	}
 	
-	handleInput = (e) => {
+	handleSearchInput = (e) => {
       this.setState({[e.target.name]: e.target.value})
     }
-
-	handleQuery = (e) => {
-	    if(e.key === 'Enter'){
-	        this.setState({isLoading: true, tweets: []})
-	        axios.get('http://localhost:3001/api/v1/customers.json?by_full_name=' + this.state.query)
-	        .then(response => {
-	          this.setState({customers: response.data})
-	        })
-	        .catch(error => console.log(error));
-	        e.preventDefault();
-	    }
+    
+	handleButtonClick = (name) => {
+		alert(name);
+		console.log("here");
 	}
+
+	handleDropdownChange = (event) => {
+	    this.setState({company: event.target.value});
+	  }
+
+	handleSubmit = (event) => {
+        axios.get('http://localhost:3001/api/v1/customers.json?by_full_name=' + this.state.query + '&by_company=' + this.state.company)
+        .then(response => {
+          this.setState({customers: response.data})
+        })
+        .catch(error => console.log(error));
+        event.preventDefault();
+	 }
 
 	render() {
 	   return (
 	   	<div>
-		   	<form>
-	          <input className='input' type="text" size="25"
-	            name="query" placeholder="Enter a search text and hit Enter"
-	            value={this.state.query} onChange={this.handleInput} onKeyPress={this.handleQuery}/>
-	         </form>
+		   	<form onSubmit={this.handleSubmit}>
+	          Search Customers: <input type="text" size="25"
+	            			name="query" placeholder="Enter customer name"
+	            			value={this.state.query} onChange={this.handleSearchInput}/>
+		        <label> Filter by Company:
+		          <select value={this.state.company} onChange={this.handleDropdownChange}>
+		            <option value="*">All</option>
+		            <option value="Dolby">Dolby</option>
+		            <option value="Apple">Apple</option>
+		            <option value="Tesla">Tesla</option>
+		          </select>
+		        </label> 
+
+		        <input type="submit" value="Submit" />
+		     </form>
+
 	         <br />
-	 	      <div>
-		         {this.state.customers.map((customer) => {
-			        return(
-			          <div className="user" key={customer.id} >
-				          <table>
-					          <tr>
-					            <td>{customer.firstname}</td>
-					            <td>{customer.lastname}</td>
-					          </tr>
-				          </table>
-			          </div>
-			        )       
+	        <table align="center">
+	          <thead>
+			    <tr>
+			      <th>Firstname</th>
+			      <th>Lastname</th>
+			      <th>Company</th>
+			      <th>Share</th>
+			      <th colSpan="4"></th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  	 {this.state.customers.map((customer) => {
+			  	 	return (
+		             <tr key={customer.id}>
+			            <td>{customer.firstname}</td>
+			            <td>{customer.lastname}</td>
+			            <td>{customer.company_id}</td>
+			            <td><button onClick={() => {this.handleButtonClick(customer.firstname)}}>Share
+    						</button>
+    					</td>
+			          </tr>
+			        )
 			      })}
-		      </div>
+		      </tbody>
+	        </table>
 		 </div>
 	    )
 	 }
